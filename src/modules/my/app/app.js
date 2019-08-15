4970029b9628fbd408d67c1f27220a53f4d948fe
Anny He <anny.he@salesforce.com>
2019-08-14 18:10:18 -0700

    shows all questions and answers

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,3 +1,29 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    all_questions = [{
+        question_string: "What color is the sky?",
+        choices: {
+          correct: "Blue",
+          all: ["Pink", "Orange", "Blue", "Green"]
+        }
+      }, {
+        question_string: "Which season is the hottest?",
+        choices: {
+          correct: "Summer",
+          all: ["Winter", "Autumn", "Spring", "Summer"]
+        }
+      }, {
+        question_string: "How many wheels are there on a tricycle?",
+        choices: {
+          correct: "Three",
+          all: ["One", "Two", "Three", "Four"]
+        }
+      }, {
+        question_string: 'Who is the main character of Harry Potter?',
+        choices: {
+          correct: "Harry Potter",
+          all: [ "Harry Potter", "Hermione Granger", "Ron Weasley", "Voldemort"]
+        }
+      }];
+}
--- src/modules/my/greeting/greeting.js
+++ src/modules/my/greeting/greeting.js
@@ -1,11 +1,16 @@
 import { LightningElement, track, api } from 'lwc';
 
-export default class Greeting extends LightningElement { 
+export default class Greeting extends LightningElement {
     @track showResult;
     @api codepenID = 'rNBeOwV';
     get resultUrl() {
         const suffix = this.showResult ? ',result' : '';
-        return '//codepen.io/annyhe/embed/' + this.codepenID + '/?height=265&theme-id=0&default-tab=js' + suffix;
+        return (
+            '//codepen.io/annyhe/embed/' +
+            this.codepenID +
+            '/?height=265&theme-id=0&default-tab=js' +
+            suffix
+        );
     }
 
     seeAnswer() {
