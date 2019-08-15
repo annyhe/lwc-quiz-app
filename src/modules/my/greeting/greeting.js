8ab489ce26205f2d612cdeffedbcf93cdd059c44
Anny He <anny.he@salesforce.com>
2019-08-14 17:18:14 -0700

    initial commit

---
+++ src/modules/my/greeting/greeting.js
@@ -0,0 +1,14 @@
+import { LightningElement, track, api } from 'lwc';
+
+export default class Greeting extends LightningElement { 
+    @track showResult;
+    @api codepenID = 'rNBeOwV';
+    get resultUrl() {
+        const suffix = this.showResult ? ',result' : '';
+        return '//codepen.io/annyhe/embed/' + this.codepenID + '/?height=265&theme-id=0&default-tab=js' + suffix;
+    }
+
+    seeAnswer() {
+        this.showResult = true;
+    }
+}
